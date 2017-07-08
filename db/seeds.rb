0f73b5bb08b6a07c7447e79f0e2a6bc27a0ddfdc
Mohamed Warsame <mwarsame9@gmail.com>
2017-07-07 18:42:42 -0700

    fix routing so can view product page

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Product.destroy_all
 50.times do |index|
   Product.create!(name: Faker::Food.ingredient,
                   cost: Faker::Commerce.price,
-                  country: Faker::Address.country
+                  country: Faker::Address.country)
 end
 
 p "Created #{Product.count} products"
